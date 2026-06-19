cef9f65d09059d29f764dbd73aadc12695034a76
joohone570 <kjhgy7738@gmail.com>
2026-06-19 20:06:12 +0900

    modify

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import flet as ft
 import duckdb
 
@@ -15,12 +14,12 @@ def main(page: ft.Page):
     # DB 생성
     # -------------------------------
 
-    con.execute("DROP TABLE IF EXISTS frame")
-    con.execute("DROP TABLE IF EXISTS primary_skill")
-    con.execute("DROP TABLE IF EXISTS character")
+    con.execute("DROP TABLE IF EXISTS move_frame")
+    con.execute("DROP TABLE IF EXISTS move")
+    con.execute("DROP TABLE IF EXISTS fighter")
 
     con.execute("""
-        CREATE TABLE IF NOT EXISTS character (
+        CREATE TABLE IF NOT EXISTS fighter (
             id BIGINT PRIMARY KEY,
             name VARCHAR,
             nationality VARCHAR,
@@ -30,142 +29,140 @@ def main(page: ft.Page):
     """)
 
     con.execute("""
-        INSERT OR IGNORE INTO character
-        SELECT * FROM read_csv_auto('data/character.csv')
+        INSERT OR IGNORE INTO fighter
+        SELECT * FROM read_csv_auto('data/fighter.csv')
     """)
 
     con.execute("""
-        CREATE TABLE IF NOT EXISTS primary_skill (
+        CREATE TABLE IF NOT EXISTS move (
             id BIGINT PRIMARY KEY,
-            character_id BIGINT,
-            skill_name VARCHAR,
+            fighter_id BIGINT,
+            move_name VARCHAR,
             command VARCHAR,
-            FOREIGN KEY(character_id)
-            REFERENCES character(id)
+            FOREIGN KEY(fighter_id)
+            REFERENCES fighter(id)
         )
     """)
 
     con.execute("""
-        INSERT OR IGNORE INTO primary_skill
+        INSERT OR IGNORE INTO move
         SELECT *
-        FROM read_csv_auto('data/primary_skill.csv')
+        FROM read_csv_auto('data/move.csv')
     """)
 
     con.execute("""
-        CREATE TABLE IF NOT EXISTS frame (
+        CREATE TABLE IF NOT EXISTS move_frame (
             id BIGINT PRIMARY KEY,
-            skill_id BIGINT,
-            startup_frame INT,
-            On_Hit INT,
-            On_Block INT,
-            FOREIGN KEY(skill_id)
-            REFERENCES primary_skill(id)
+            move_id BIGINT,
+            startup INT,
+            Hit INT,
+            Block INT,
+            Counter INT,
+            FOREIGN KEY(move_id)
+            REFERENCES move(id)
         )
     """)
 
     con.execute("""
-        INSERT OR IGNORE INTO frame
+        INSERT OR IGNORE INTO move_frame
         SELECT *
-        FROM read_csv_auto('data/frame.csv')
+        FROM read_csv_auto('data/move_frame.csv')
     """)
 
     # -------------------------------
     # 조회 함수
     # -------------------------------
 
-    def get_character(character_id):
+    def get_character(fighter_id):
         return con.execute("""
         SELECT *
-        FROM character
+        FROM fighter
         WHERE id=?
-    """, [character_id]).fetchone()
+    """, [fighter_id]).fetchone()
 
-    def get_skills(character_id):
+    def get_skills(fighter_id):
         return con.execute("""
         SELECT *
-        FROM primary_skill
-        WHERE character_id=?
-    """, [character_id]).fetchall()
+        FROM move
+        WHERE fighter_id=?
+    """, [fighter_id]).fetchall()
 
-    def get_frame(skill_id):
+    def get_frame(move_id):
         return con.execute("""
         SELECT *
-        FROM frame
-        WHERE skill_id=?
-    """, [skill_id]).fetchone()
+        FROM move_frame
+        WHERE move_id=?
+    """, [move_id]).fetchone()
 
-    current_character_id = [1]
-    current_character_name = ["카즈야"]
+    current_fighter_id = [1]
+    current_fighter_name = ["카즈야"]
 
     info_area = ft.Column()
-    skill_area = ft.Column()
-    frame_area = ft.Column()
-    character_image = ft.Image(
+    move_area = ft.Column()
+    move_frame_area = ft.Column()
+    fighter_image = ft.Image(
         src="",
         width=200,
         height=200
-)
+    )
 
     def show_info(e):
-        character = get_character(current_character_id[0])
+        fighter = get_character(current_fighter_id[0])
         info_area.controls.clear()
         info_area.controls.extend([
-            ft.Text(f"국적 : {character[2]}"),
-            ft.Text(f"격투 스타일 : {character[3]}")
+            ft.Text(f"국적 : {fighter[2]}"),
+            ft.Text(f"격투 스타일 : {fighter[3]}")
         ])
         page.update()
 
     def show_frame(skill_id):
-        frame = get_frame(skill_id)
-        frame_area.controls.clear()
-        frame_area.controls.extend([
-            ft.Text(f"Startup Frame : {frame[2]}"),
-            ft.Text(f"On Hit : {frame[3]}"),
-            ft.Text(f"On Block : {frame[4]}")
+        move_frame = get_frame(skill_id)
+        move_frame_area.controls.clear()
+        move_frame_area.controls.extend([
+            ft.Text(f"Startup : {move_frame[2]}"),
+            ft.Text(f"Hit : {move_frame[3]}"),
+            ft.Text(f"Block : {move_frame[4]}"),
+            ft.Text(f"Counter : {move_frame[5]}")
         ])
         page.update()
 
     def show_skills(e):
-        skills = get_skills(current_character_id[0])
+        moves = get_skills(current_fighter_id[0])
 
-        skill_area.controls.clear()
+        move_area.controls.clear()
 
-        for skill in skills:
-            skill_area.controls.append(
-                ft.Container(
-                    content=ft.Column([
-                        ft.Text(
-                            f"기술명 : {skill[2]}"
-                        ),
-                        ft.Text(
-                            f"커맨드 : {skill[3]}"
-                        ),
-                        ft.ElevatedButton(
-                            "프레임 보기",
-                            on_click=lambda e, sid=skill[0]:
-                            show_frame(sid)
-                        )
-                    ])
-                )
+        for skill in moves:
+            move_area.controls.append(
+                ft.Column([
+                    ft.Text(f"기술명 : {skill[2]}"),
+                    ft.Text(f"커맨드 : {skill[3]}"),
+                    ft.Button(
+                        "프레임 보기",
+                        on_click=lambda e, sid=skill[0]:
+                        show_frame(sid)
+                    ),
+                    ft.Text("----------------")
+                ])
             )
 
     page.update()
 
-    def select_character(character_id):
-        current_character_id[0] = character_id
-        character = get_character(character_id)
-        current_character_name[0] = character[1]
-        character_image.src = character[4]
+    def select_character(fighter_id):
+        current_fighter_id[0] = fighter_id
+        fighter = get_character(fighter_id)
+        current_fighter_name[0] = fighter[1]
+        fighter_image.src = fighter[4]
         info_area.controls.clear()
-        skill_area.controls.clear()
-        frame_area.controls.clear()
+        move_area.controls.clear()
+        move_frame_area.controls.clear()
         page.update()
 
     character_panel = ft.Container(
         content=ft.Column([
             ft.Text("캐릭터"),
             ft.Button("카즈야", on_click=lambda e: select_character(1)),
-            ft.Button("진", on_click=lambda e: select_character(2))
+            ft.Button("진", on_click=lambda e: select_character(2)),
+            ft.Button("빅터",on_click=lambda e: select_character(3))
         ])
     )
 
@@ -179,7 +176,7 @@ def main(page: ft.Page):
         ft.Container(content=info_area)
     ])
 
-    skill_panel = ft.Row([
+    move_panel = ft.Row([
         ft.Container(
             content=ft.Column([
                 ft.Text("주력기"),
@@ -189,26 +186,25 @@ def main(page: ft.Page):
         ft.Container(
             width=350,
             height=200,
-            content=ft.ListView(controls=[skill_area])
+            content=ft.ListView(controls=[move_area])
         )
     ])
 
-    frame_panel = ft.Row([
-        ft.Container(width=350, content=frame_area)
+    move_frame_panel = ft.Row([
+        ft.Container(width=350, content=move_frame_area)
     ])
-
 
     page.add(
         ft.Text("4.1 캐릭터 선택 조회"),
         character_panel,
         ft.Text("캐릭터 이미지"),
-        character_image,
+        fighter_image,
         ft.Text("4.2 캐릭터 정보 조회"),
         character_info_panel,
-        ft.Text("4.3 주력기 정보 조회"),
-        skill_panel,
-        ft.Text("4.4 주력기 프레임 정보 조회"),
-        frame_panel
+        ft.Text("4.3 기술 정보 조회"),
+        move_panel,
+        ft.Text("4.4 기술 프레임 정보 조회"),
+        move_frame_panel
     )
 
     select_character(1)
